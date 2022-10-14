70cc38d21d9e65d86ddc4dadbd6bd9b9f7eda5b5
hailatGH <hailat.alx@gmail.com>
2022-10-14 15:13:27 +0300

    all api routes are set and ready for db connection

--- server/server.js
+++ server/server.js
@@ -1,13 +1,62 @@
 require('dotenv').config();
 
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-app.get('/getRestaurants', (req, res) => {
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Get All Restaurants
+app.get('/api/v1/restaurants', (req, res) => {
     res.json({
         status: "Success",
-        restaurant: "Mcdonalds"
-    })
+        data:{
+            restaurants: ['Macdonalds', 'Wendys'],
+        },
+    });
+});
+
+// Get individual Restaurants
+app.get('/api/v1/restaurants/:id', (req, res) => {
+    console.log(req.params);
+    res.status(200).json({
+        status: "Success",
+        data:{
+            restaurants: ['Macdonalds'],
+        },
+    });
+});
+
+// Create a Restaurant
+app.post('/api/v1/restaurants', (req, res) => {
+    console.log(req.body);
+    res.status(201).json({
+        status: "Success",
+        data:{
+            restaurants: ['Macdonalds'],
+        },
+    });
+});
+
+// Update The Restaurant
+app.put('/api/v1/restaurants/:id', (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
+    res.status(200).json({
+        status: "Success",
+        data:{
+            restaurants: ['Macdonalds'],
+        },
+    });
+});
+
+// Delete A Restaurant
+app.delete('/api/v1/restaurants/:id', (req, res) => {
+    res.status(204).json({
+        status: "Success",
+    });
 });
 
 const port = process.env.PORT || 3001;
